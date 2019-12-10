3cc30a416171ccef51f1a30516f459626e2ac4e2
Edmil Di Massimo <edmildm@gmail.com>
2019-12-10 15:15:37 -0400

    Expenses and incomes by user id

--- resolvers/query.js
+++ resolvers/query.js
@@ -7,12 +7,20 @@ function users(root, args, context, info) {
 
 function expenses(root, args, context, info) {
   utils.requestAuth(context);
-  return context.prisma.expenses();
+  if (args.filter) {
+    return context.prisma.user({id: args.filter}).expenses();
+  } else {
+    return context.prisma.expenses();
+  }
 };
 
 function incomes(root, args, context, info) {
   utils.requestAuth(context);
-  return context.prisma.incomes();
+  if (args.filter) {
+    return context.prisma.user({id: args.filter}).income();
+  } else {
+    return context.prisma.incomes();
+  }
 };
 
 module.exports = {
