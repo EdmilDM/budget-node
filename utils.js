45b7175a5075da5915c8c4a24bf5f4e372c0c2cd
Edmil Di Massimo <edmildm@gmail.com>
2019-12-10 16:14:32 -0400

    Fixed env issues.

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ function requestAuth(context) {
   const Authorization = context.request.get('Authorization');
   if (Authorization) {
     const token = Authorization.replace('Token ', '');
-    const {userId} = jwt.verify(token, process.env.APP_SECRET);
+    const {userId} = jwt.verify(token, env.APP_SECRET);
     return userId;
   }
 
